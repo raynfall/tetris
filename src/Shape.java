93bb1d33be878149bce7fd5de6b5e1f6ca39a62a
Ray <raymond.tran1204@gmail.com>
2018-06-03 21:28:20 -0500

    Initial commit

---
+++ src/Shape.java
@@ -0,0 +1,13 @@
+abstract class Shape {
+
+    String shapeName = "";
+    Double fallSpeed = 10.0;
+    Double gravityIncrease = 0.5;
+
+    Shape(String shapeName) {
+        this.shapeName = shapeName;
+    }
+
+    
+
+}
